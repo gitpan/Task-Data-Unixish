45832b442fa81a43e81c3ad82a6a8ccf782eed4e
Steven Haryanto <stevenharyanto@gmail.com>
2012-07-13 12:30:54 +0000

    Import of SHARYANTO/Task-Data-Unixish-0.01 from CPAN.
    gitpan-cpan-distribution: Task-Data-Unixish
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         SHARYANTO/Task-Data-Unixish-0.01.tar.gz
    gitpan-cpan-author:       SHARYANTO
    gitpan-cpan-maturity:     released

---
+++ lib/Task/Data/Unixish.pm
@@ -0,0 +1,44 @@
+package Task::Data::Unixish;
+
+our $VERSION = '0.01'; # VERSION
+
+1;
+# ABSTRACT: Install all Data::Unixish modules
+
+
+__END__
+=pod
+
+=head1 NAME
+
+Task::Data::Unixish - Install all Data::Unixish modules
+
+=head1 VERSION
+
+version 0.01
+
+=head1 DESCRIPTION
+
+This task will install all Data::Unixish modules.
+
+=head1 TASK CONTENTS
+
+=head2 Included modules
+
+=head3 L<Data::Unixish>
+
+=head3 L<Data::Unixish::Implementation>
+
+=head1 AUTHOR
+
+Steven Haryanto <stevenharyanto@gmail.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2012 by Steven Haryanto.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
+
